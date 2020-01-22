cece6a6945633b3e256bc4c38c8eeca142c94c2c
Alexander Brand <alexbrand09@gmail.com>
2020-01-22 10:47:35 -0500

    Update/fix dependencies. Delete unnecessary files
    Signed-off-by: Alexander Brand <alexbrand09@gmail.com>

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 const express = require('express');
 const cors = require('cors');
 
@@ -8,9 +7,17 @@ app.use(cors());
 
 const port = 8001;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/api/products', (req, res) => {
   res.sendFile(path.join(__dirname, 'data', 'products.json'));
 });
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`[products] API listening on port ${port}.`);
